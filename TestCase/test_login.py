a441fc7329667f196ea7bcd60e74d5e8c8373e82
韩继平 <han0801@163.com>
2021-11-30 22:32:55 +0800

    增加前后置

--- TestCase/test_login.py
+++ TestCase/test_login.py
@@ -5,6 +5,20 @@ class Test_Log(unittest.TestCase):
     def test_login(self):
         assert 1 == 1
 
+    def setUp(self) -> None:
+        print("用例开始执行")
+
+    def tearDown(self) -> None:
+        print("用例执行完毕")
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        pass
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
